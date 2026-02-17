9f078f309ae9b37ba24b617f891935b29fd7d320
Rishad05 <iu.rishad5@gmail.com>
2026-02-17 12:25:14 +0600

    feat: build SwiftCart frontend with dynamic product filtering and responsive UI
    Create responsive homepage using Tailwind CSS
    
    Implement sticky navbar with mobile menu toggle
    
    Add hero section with CTA button
    
    Build Why Choose Us feature section
    
    Design reusable footer with newsletter subscription
    
    Develop products page with category filter pills
    
    Integrate FakeStore API for fetching products
    
    Render products dynamically using DOM manipulation
    
    Implement category-wise product filtering
    
    Add default All category handling
    
    Integrate Font Awesome icons for UI enhancement
    
    Ensure responsive grid layout for all screen sizes

---
+++ main.js
@@ -0,0 +1,119 @@
+// load all products
+
+const loadProducts = () => {
+ const url = "https://fakestoreapi.com/products";
+ console.log("the url is", url);
+ fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+   displayProducts(data);
+  });
+};
+
+// display all products use tailwind
+const displayProducts = (products) => {
+ const productsContainer = document.getElementById("productsContainer");
+ productsContainer.innerHTML = "";
+ products.forEach((product) => {
+  const productDiv = document.createElement("div");
+
+  productDiv.innerHTML = `
+   <div class="bg-white rounded-xl shadow-sm p-4 hover:shadow-lg transition">
+     <img src="${product.image}" class="w-full h-60 object-cover rounded-lg mb-4" />
+     <h4 class="font-semibold mb-2">${product.title}</h4>
+     <p class="text-indigo-600 font-bold mb-4">$${product.price}</p>
+     <div class="flex justify-between">
+      <button class="border px-4 py-2 rounded-lg text-sm">
+       <i class="fa-solid fa-eye mr-1"></i> Details
+      </button>
+      <button class="bg-indigo-600 text-white px-4 py-2 rounded-lg text-sm">
+       <i class="fa-solid fa-cart-plus mr-1"></i> Add
+      </button>
+     </div>
+    </div>
+    
+  `;
+  productsContainer.appendChild(productDiv);
+ });
+};
+
+
+//all categories
+const allCategories = () => {
+ const url = "https://fakestoreapi.com/products/categories";
+ fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+   const categories = data;
+   displayCategories(categories);
+  });
+}
+const displayCategories = (categories) => {
+ const categoriesContainer = document.getElementById("categoriesContainer");
+ categoriesContainer.innerHTML = "";
+
+ const sortedCategories = ["all", ...categories.filter(c => c !== "all")];
+
+ sortedCategories.forEach((category) => {
+  const button = document.createElement("button");
+
+    button.className = `capitalize ${
+      category === "all"
+        ? "bg-indigo-600 text-white"
+        : "bg-white text-indigo-600"
+    } px-4 py-2 rounded-lg text-sm hover:bg-indigo-700`;
+
+    button.textContent = category === "all" ? "All" : category;
+
+    if (category === "all") {
+      button.addEventListener("click", loadProducts);
+    } else {
+      button.addEventListener("click", () => filterProducts(category));
+    }
+
+    categoriesContainer.appendChild(button);
+ });
+
+}
+allCategories();
+
+// category filter
+const filterProducts = (category) => {
+ const url = `https://fakestoreapi.com/products/category/${category}`;
+ console.log("the url is", url);
+ fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+   catWiseProducts(data);
+  });
+};
+
+//display category wise products
+const catWiseProducts = (products) => {
+ const productsContainer = document.getElementById("productsContainer");
+ productsContainer.innerHTML = "";
+ products.forEach((product) => {
+  const productDiv = document.createElement("div");
+
+  productDiv.innerHTML = `
+   <div class="bg-white rounded-xl shadow-sm p-4 hover:shadow-lg transition">
+     <img src="${product.image}" class="w-full h-60 object-cover rounded-lg mb-4" />
+     <h4 class="font-semibold mb-2">${product.title}</h4>
+     <p class="text-indigo-600 font-bold mb-4">$${product.price}</p>
+     <div class="flex justify-between">
+      <button class="border px-4 py-2 rounded-lg text-sm">
+       <i class="fa-solid fa-eye mr-1"></i> Details
+      </button>
+      <button class="bg-indigo-600 text-white px-4 py-2 rounded-lg text-sm">
+       <i class="fa-solid fa-cart-plus mr-1"></i> Add
+      </button>
+     </div>
+    </div>
+    
+  `;
+  productsContainer.appendChild(productDiv);
+ });
+
+}
+loadProducts();
+filterProducts("All");
